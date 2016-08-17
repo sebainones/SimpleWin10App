9707674395d7a6d733e6adfeb10301b699f4d298
sebainones <seba.inones@gmail.com>
2016-08-17 18:55:26 +0200

    AdControl and some layout change

--- App1/App.xaml.cs
+++ App1/App.xaml.cs
@@ -58,7 +58,7 @@ namespace RateApp
 
         private static void SetApplicationSize()
         {
-            ApplicationView.PreferredLaunchViewSize = new Windows.Foundation.Size(680, 740);
+            ApplicationView.PreferredLaunchViewSize = new Windows.Foundation.Size(540, 500);
             ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
         }
 
