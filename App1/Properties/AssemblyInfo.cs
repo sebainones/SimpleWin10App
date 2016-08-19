10b59e45bf2a26d41d751372124d9536955e697c
sebainones <seba.inones@gmail.com>
2016-08-19 20:29:39 +0200

    Preparing the app for being published. Some changes on the package and manifest and on the second view.

--- App1/Properties/AssemblyInfo.cs
+++ App1/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RateApp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Porvee la cotización del dolar y el euro en argentina actualziada diariamente.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Digital Hope Software")]
 [assembly: AssemblyProduct("RateApp")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -27,3 +28,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("es")]
+
